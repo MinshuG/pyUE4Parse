7c2a7df0a9b2aba5b5df22880d3c716be2ed5973
MinshuG <65584814+MinshuG@users.noreply.github.com>
2021-05-05 20:19:44 +0530

    fixpath fix

--- UE4Parse/provider/utils.py
+++ UE4Parse/provider/utils.py
@@ -1,8 +1,8 @@
-from UE4Parse.Globals import FGame
+from UE4Parse import Globals as glob
 
 
 def fixpath(path: str) -> str:
-    GameName = FGame.GameName
+    GameName = glob.FGame.GameName
     if path.startswith("/Game/"):
         path = path.replace("/Game/", GameName + "/Content/")
 
