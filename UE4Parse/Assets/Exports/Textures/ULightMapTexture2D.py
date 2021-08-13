bf91fc21c9b10cf2a8dabbd5a1774fbd6a8d24c8
MountainFlash <65584814+MinshuG@users.noreply.github.com>
2021-08-13 11:41:17 +0530

    ULightMapTexture2D

--- UE4Parse/Assets/Exports/Textures/ULightMapTexture2D.py
+++ UE4Parse/Assets/Exports/Textures/ULightMapTexture2D.py
@@ -1,7 +1,36 @@
+from enum import IntEnum
+from typing import Union
+
 from UE4Parse.Assets.Exports.Textures import UTexture2D
 from UE4Parse.Assets.Exports.ExportRegistry import register_export
+from contextlib import suppress
+
+
+class ELightMapFlags(IntEnum):
+    """
+    Bit-field flags that affects storage (e.g. packing, streaming) and other info about a lightmap.
+    """
+    LMF_None = 0  # No flags
+    LMF_Streamed = 0x00000001  # Lightmap should be placed in a streaming texture
 
 
 @register_export
 class ULightMapTexture2D(UTexture2D):
-    pass
+    """A 2D texture containing lightmap coefficients."""
+    LightmapFlags: Union[int, ELightMapFlags]
+
+    def __init__(self, reader):
+        super().__init__(reader)
+
+    def deserialize(self, validpos):
+        super().deserialize(validpos)
+        reader = self.reader
+        self.LightmapFlags = reader.readUInt32()
+
+        with suppress(ValueError):
+            self.LightmapFlags = ELightMapFlags(self.LightmapFlags)
+
+    def GetValue(self):
+        props = super().GetValue()
+        props["LightmapFlags"] = self.LightmapFlags.value if isinstance(self.LightmapFlags, IntEnum) else self.LightmapFlags
+        return props
